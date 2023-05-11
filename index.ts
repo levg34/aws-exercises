dd665193cdf7b99622671bd111d48a1535fd15ef
levg34 <levg34@users.noreply.github.com>
2023-05-11 18:44:58 +0000

    list s3 buckets

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
+import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3"
+
 console.log('Bonjour')
+
+const main = async () => {
+    const client = new S3Client({ region: "ap-south-1" })
+    
+    const command = new ListBucketsCommand({})
+    const response = await client.send(command);
+    
+    console.info(response.Buckets)
+}
+
+main()
